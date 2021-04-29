65d902e501bc120ea526f5e55b13db80f464b362
Mac Heller-Ogden <mac@machellerogden.com>
2021-04-29 17:13:14 -0500

    feat: quick gitlab support -- sorry i am not going to bother updating deps or doing any other house-keeping at this time

---
+++ drivers/gitlab/index.js
@@ -0,0 +1,29 @@
+'use strict';
+const _ = require('lodash');
+const axios = require('axios');
+const Promise = require('bluebird');
+
+module.exports =  {
+    getConfiguredRepos
+};
+
+function getConfiguredRepos(config) {
+    const gitlabConfig = _.find(config.drivers, { type: 'gitlab' });
+    if (gitlabConfig) {
+        const { privateToken, groups } = gitlabConfig;
+        return Promise.reduce(groups, (acc, groupId) =>
+            axios.request({
+                url: `https://gitlab.com/api/v4/groups/${groupId}/projects`,
+                headers: { 'PRIVATE-TOKEN': privateToken }
+            }).then((res) => [
+                ...acc,
+                ...res.data.map(v => ({
+                    name: v.path_with_namespace,
+                    clone: v.http_url_to_repo,
+                    html: v._links.self
+                }))
+            ]), []);
+    } else {
+        return Promise.resolve([]);
+    }
+}
--- drivers/index.js
+++ drivers/index.js
@@ -1,3 +1,4 @@
 'use strict';
 exports['bitbucket-server'] = require('./bitbucket-server');
 exports['github'] = require('./github');
+exports['gitlab'] = require('./gitlab');
